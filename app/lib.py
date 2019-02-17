a19d33f19a8ad5dd9d80e49f10069bc7933dbdf3
xabuk44 <xabuk44@gmail.com>
2019-02-17 18:33:04 +0300

    added code to main and lib create - git, requirements, .gitignore, github repo

---
+++ app/lib.py
@@ -0,0 +1,17 @@
+blue_percent = 0.05
+silver_percent = 0.07
+golden_percent = 0.1
+blue_purchase_max = 15_000
+silver_purchase_max = 150_000
+coefficient = 1000 #бонусы начисляются на каждую 1000 рублей,
+                   #соответственно вводится коэффициент для коррекции
+
+
+def s_m_bonus(full_purchase, last_purchase):
+    if full_purchase < blue_purchase_max:
+        bonus = (int(last_purchase/coefficient) * blue_percent) * coefficient
+    if blue_purchase_max < full_purchase < silver_purchase_max:
+        bonus = (int(last_purchase/coefficient) * silver_percent) * coefficient
+    if full_purchase > silver_purchase_max:
+        bonus = (int(last_purchase/coefficient) * golden_percent) * coefficient
+    return bonus
---
+++ app/main_sm.py
@@ -0,0 +1,6 @@
+from lib import s_m_bonus
+
+
+print(s_m_bonus(10_000, 4_700))
+print(s_m_bonus(80_000, 1_200))
+print(s_m_bonus(160_000, 4_600))
